9aef33d9e9c71c8302d5e44d01293b93638990c3
Enes Ünlü <96426063+Enesunlu@users.noreply.github.com>
2022-12-06 17:29:58 +0300

    Task 3
    -- Q1
    -- select country from country
    -- where country like 'A%a';
    
    -- Q2
    -- select country from country
    -- where country like '_____%n';
    
    -- Q3
    -- select title from film 
    -- where title ILIKE '%T&T&T&T&';
    
    -- Q4
    -- select * from film
    -- where title ILIKE 'C%' and length > 90 and rental_rate=2.99;

---
+++ Task3.sql
@@ -0,0 +1,15 @@
+-- Q1
+-- select country from country
+-- where country like 'A%a';
+
+-- Q2
+-- select country from country
+-- where country like '_____%n';
+
+-- Q3
+-- select title from film 
+-- where title ILIKE '%T&T&T&T&';
+
+-- Q4
+-- select * from film
+-- where title ILIKE 'C%' and length > 90 and rental_rate=2.99;
